a9cbbeec748ec01d1b7223e68a20a8b8f7cd48c3
gxy <a597966>
2025-09-03 23:24:39 +0800

    页面逻辑优化

--- miniprogram/pages/compute/simpleAddSub/index.ts
+++ miniprogram/pages/compute/simpleAddSub/index.ts
@@ -11,6 +11,13 @@ Page({
     correctAnswer: 0,
     currentIndex: 0,
     userAnswer : "",
+    inputFocus : false,
+
+    lastInputTime: 0, // 上一次输入的时间戳
+    timer: 0, // 定时器
+    debounce_time : 700,
+
+    errorShake : false,
   },
 
   /**
@@ -45,7 +52,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    // 页面卸载时清除定时器
+    clearTimeout(this.data.timer);
   },
 
   /**
@@ -97,31 +105,64 @@ Page({
     if (isAddition) {
         num1 = Math.floor(Math.random() * this.data.currentRange) + 1;
         num2 = Math.floor(Math.random() * (this.data.currentRange - num1)) + 1;
-        _currentProblem = `${num1} + ${num2} = ?`;
+        _currentProblem = `${num1} + ${num2} = `;
         _correctAnswer = num1 + num2;
         
     } else {
         num1 = Math.floor(Math.random() * this.data.currentRange) + 1;
         num2 = Math.floor(Math.random() * (num1));
-        _currentProblem = `${num1} - ${num2} = ?`;
+        _currentProblem = `${num1} - ${num2} = `;
         _correctAnswer = num1 - num2;
     }
     
     this.setData({
       currentProblem: _currentProblem,
-      correctAnswer: _correctAnswer
+      correctAnswer: _correctAnswer,
+      inputFocus : true
     })
   },
-  checkAnswer(e){
-    //记录两次输入时间，若大于1000毫秒，可认为是确认了答案，相当于点了提交，自动判定答案，若当前值与答案相同，这个时间可以缩小到400毫秒，直接显示正确，然后进入下一题。
+  inputChange(e){
     const _userAnswer = parseInt(e.detail.value)
-    if (isNaN(_userAnswer)) {
+    
+     // 清除之前的定时器
+     if (this.data.timer) {
+          clearTimeout(this.data.timer);
+      }
+    // 设置新的定时器
+    var debounceTimer = setTimeout(() => {
+            this.checkAnswer()
+        }, this.data.debounce_time);
+  this.setData({
+    userAnswer: _userAnswer,
+    timer:debounceTimer
+  })
+
+  },
+  nextProblem(){
+    this.checkAnswer()
+  },
+  //检测答案
+  checkAnswer(){
+    if (isNaN(this.data.userAnswer)) {
       this.setData({
         userAnswer:""
       })
-    }else if (_userAnswer === this.data.correctAnswer) {
+    }else if (this.data.userAnswer === this.data.correctAnswer) {
+      this.nextProblem()
+    }else{
+      console.log("no")
+      this.setData({errorShake: true})
+
+      setTimeout(() => {  this.setData({errorShake: false});this.nextProblem(); }, 800)
       
     }
+  },
+  //下一题，记录刚才的是否正确
+  nextProblem(){
+    this.setData({
+      userAnswer:""
+    })
+    this.generateNewProblem()
   }
 //   // 检查答案
 //   checkAnswer() {
